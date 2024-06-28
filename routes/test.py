f258409c4586e2935311e59facc4779f211f4f66
Jorn Kalu <kiceej@gmail.com>
2024-06-28 16:56:41 +0100

    Increase response

--- routes/test.py
+++ routes/test.py
@@ -20,7 +20,8 @@ async def post_test(request: Request, db: Session = Depends(get_session)):
     client_host = request.client.host
     body = await request.body()
     create_log(db=db, ip_address=client_host, data=body)
-    data = "R311100010000010030911202405181612098"
+    # data = "R311100010000010030911202405181612098"
+    data = "R31110001000001003091120240518161209819383728463728818643875"
     return Response(content=data, media_type="text/plain")
 
 @router.post("/form_test")
@@ -28,4 +29,5 @@ async def form_test(request: Request, db: Session = Depends(get_session), body: 
     client_host = request.client.host
     create_log(db=db, ip_address=client_host, data=body)
     data = "R311100010000010030911202405181612098"
+    data = "R31110001000001003091120240518161209819383728463728818643875"
     return Response(content=data, media_type="text/plain")
